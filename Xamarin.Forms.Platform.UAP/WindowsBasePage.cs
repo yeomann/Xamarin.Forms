2c97a0d9241c8c549aab684cfc99baec52678486
E.Z. Hart <hartez@gmail.com>
2018-07-25 09:08:28 -0600

    Rolling back platform test on UWP

--- Xamarin.Forms.Platform.UAP/WindowsBasePage.cs
+++ Xamarin.Forms.Platform.UAP/WindowsBasePage.cs
@@ -27,8 +27,6 @@ namespace Xamarin.Forms.Platform.UWP
 			Application.SetCurrentApplication(application);
 			Platform = CreatePlatform();
 
-			VisualElement.Platform2 = Platform;
-
 			Platform.SetPage(Application.Current.MainPage);
 			application.PropertyChanged += OnApplicationPropertyChanged;
 
